8b0195e42bb1c9b3c8b778ee1cc40afd83668d04
User <user@example.com>
2025-10-24 07:47:13 +0900

    fix(build): Explicitly type authOptions to fix type error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
+import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
