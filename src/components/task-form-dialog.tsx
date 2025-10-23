c0ae69eed74412021e63651285880d419384c9df
User <user@example.com>
2025-10-24 07:44:06 +0900

    fix(build): Add 'use client' to task-form-dialog

--- src/components/task-form-dialog.tsx
+++ src/components/task-form-dialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
